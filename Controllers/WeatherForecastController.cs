814260126efe38691c62305e283dd15a762aaa70
jrest01 <julian11072000@hotmail.com>
2024-06-05 13:09:54 -0500

    Adding the basics routes

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace NetAPI.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
@@ -13,21 +13,61 @@ namespace NetAPI.Controllers
 
         private readonly ILogger<WeatherForecastController> _logger;
 
+        private static List<WeatherForecast> ListWeatherForecast = new List<WeatherForecast>();
+
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
+            if (ListWeatherForecast == null || !ListWeatherForecast.Any())
+            {
+                ListWeatherForecast = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+            .ToList();
+            }
+
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return ListWeatherForecast;
+        }
+
+        [HttpGet]
+        [Route("Get/WeatherForecast")]
+        [Route("[action]")]
+        public IEnumerable<WeatherForecast> Get(int id)
+        {
+            return ListWeatherForecast;
+        }
+
+        [HttpPost]
+        public IActionResult Post(WeatherForecast weatherForecast)
+        {
+            ListWeatherForecast.Add(weatherForecast);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Edit(int id, WeatherForecast weatherForecast)
+        {
+            var item = ListWeatherForecast[id];
+            item.Summary = weatherForecast.Summary;
+            item.TemperatureC = weatherForecast.TemperatureC;
+            item.Date = weatherForecast.Date;
+            return Ok(item);
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            ListWeatherForecast.RemoveAt(id);
+            return Ok();
         }
     }
 }
